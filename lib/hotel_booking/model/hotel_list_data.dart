412dc69d52da5c27d743543645a1968935267176
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-08-04 18:47:12 +0530

    removed extra hotels

--- lib/hotel_booking/model/hotel_list_data.dart
+++ lib/hotel_booking/model/hotel_list_data.dart
@@ -27,41 +27,6 @@ class HotelListData {
       rating: 4.4,
       perNight: 180,
     ),
-    HotelListData(
-      imagePath: 'assets/hotel/hotel_2.png',
-      titleTxt: 'Queen Hotel',
-      subTxt: 'Wembley, London',
-      dist: 4.0,
-      reviews: 74,
-      rating: 4.5,
-      perNight: 200,
-    ),
-    HotelListData(
-      imagePath: 'assets/hotel/hotel_3.png',
-      titleTxt: 'Grand Royal Hotel',
-      subTxt: 'Wembley, London',
-      dist: 3.0,
-      reviews: 62,
-      rating: 4.0,
-      perNight: 60,
-    ),
-    HotelListData(
-      imagePath: 'assets/hotel/hotel_4.png',
-      titleTxt: 'Queen Hotel',
-      subTxt: 'Wembley, London',
-      dist: 7.0,
-      reviews: 90,
-      rating: 4.4,
-      perNight: 170,
-    ),
-    HotelListData(
-      imagePath: 'assets/hotel/hotel_5.png',
-      titleTxt: 'Grand Royal Hotel',
-      subTxt: 'Wembley, London',
-      dist: 2.0,
-      reviews: 240,
-      rating: 4.5,
-      perNight: 200,
-    ),
+
   ];
 }
